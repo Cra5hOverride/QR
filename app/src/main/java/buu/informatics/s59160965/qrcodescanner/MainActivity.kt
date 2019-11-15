7bf90e3776cbd75d4e228dffc99065e59dd0d8ae
Zerocool4444 <phichaya6310@gmail.com>
2019-11-15 23:29:05 +0700

    first commit

---
+++ app/src/main/java/buu/informatics/s59160965/qrcodescanner/MainActivity.kt
@@ -0,0 +1,31 @@
+package buu.informatics.s59160965.qrcodescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import buu.informatics.s59160965.qrcodescanner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+
+}
