df95d1a40eeb505a5aaece22ec28dd9001cfca2d
Zerocool <Phichaya6310@gmail.com>
2019-11-18 20:51:31 +0700

    scan qrcode

--- app/src/main/java/buu/informatics/s59160965/qrcodescanner/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160965/qrcodescanner/MainActivity.kt
@@ -1,17 +1,26 @@
 package buu.informatics.s59160965.qrcodescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160965.qrcodescanner.databinding.ActivityMainBinding
+import android.content.Intent
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Activity", "Welcome to  Activity")
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
@@ -26,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
     }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i("Activity", "Test Result Activity")
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val navHostFragment = supportFragmentManager.fragments.first() as? NavHostFragment
+        if(navHostFragment != null) {
+            val childFragments = navHostFragment.childFragmentManager.fragments
+            childFragments.forEach { fragment ->
+                fragment.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+
 
 
 }
--- app/src/main/java/buu/informatics/s59160965/qrcodescanner/screens/qrcodescan/qrcodescanFragment.kt
+++ app/src/main/java/buu/informatics/s59160965/qrcodescanner/screens/qrcodescan/qrcodescanFragment.kt
@@ -2,9 +2,13 @@ package buu.informatics.s59160965.qrcodescanner.screens.qrcodescan
 
 
 import android.app.Activity
-import android.content.Context
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,12 +18,17 @@ import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160965.qrcodescanner.R
 import buu.informatics.s59160965.qrcodescanner.databinding.FragmentQrcodescanBinding
 import com.google.zxing.integration.android.IntentIntegrator
-import kotlinx.android.synthetic.main.fragment_qrcodescan.*
+import java.util.regex.Pattern
+
+
+
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class qrcodescanFragment : Fragment() {
+
     private lateinit var binding: FragmentQrcodescanBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,31 +36,58 @@ class qrcodescanFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentQrcodescanBinding>(inflater,
             R.layout.fragment_qrcodescan,container,false)
-        binding.apply {
-            scanbutton.setOnClickListener{
+            binding.scanbutton.setOnClickListener{
                 val scanner = IntentIntegrator(activity)
                 scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
                 scanner.setBeepEnabled(false)
                 scanner.initiateScan()
             }
-        }
+
+
 
         return binding.root
     }
+
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i("Fragment", "Test Result")
         if (resultCode == Activity.RESULT_OK) {
             val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
             if (result != null) {
                 if (result.contents == null) {
                     Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
                 } else {
-                    Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                    var content = result.contents
+//                    Toast.makeText(context, "Scanned: " + content, Toast.LENGTH_LONG).show()
+                    copy2clipboard(content)
+                    Log.i("Link", "Scanned: " + content)
+
                 }
             } else {
                 super.onActivityResult(requestCode, resultCode, data)
             }
         }
-    }
 
+    }
+    fun copy2clipboard(text: CharSequence){
+
+        val URL_REGEX = "^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$"
+        val p = Pattern.compile(URL_REGEX)
+        val m = p.matcher(text)
+        if(m.find()){
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse(text as String?)
+            startActivity(openURL)
+        }else{
+            val clipboard = activity?.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copy text", text)
+            clipboard.setPrimaryClip(clip)
+
+            Toast.makeText(context, "Text Copied", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
 
 }
