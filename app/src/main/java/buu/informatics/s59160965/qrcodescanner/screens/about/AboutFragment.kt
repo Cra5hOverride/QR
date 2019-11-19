c6b36ee5f24f08a65a0340690753823309106fa8
Zerocool <Phichaya6310@gmail.com>
2019-11-19 19:30:20 +0700

    add share

--- app/src/main/java/buu/informatics/s59160965/qrcodescanner/screens/about/AboutFragment.kt
+++ app/src/main/java/buu/informatics/s59160965/qrcodescanner/screens/about/AboutFragment.kt
@@ -1,13 +1,16 @@
 package buu.informatics.s59160965.qrcodescanner.screens.about
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 import buu.informatics.s59160965.qrcodescanner.R
+import buu.informatics.s59160965.qrcodescanner.databinding.FragmentAboutBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,9 +21,40 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater,R.layout.fragment_about,container,false)
+        setHasOptionsMenu(true)
+
+
+        return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.result_menu, menu)
+
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
+
+//        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent() : Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,"QRcodescan by Phitchaya")
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 
 }
